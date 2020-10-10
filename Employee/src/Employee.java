a6948706175198604626f10106010f18b7aaca22
Anshika1024 <70636077+Anshika1024@users.noreply.github.com>
2020-10-10 19:55:31 +0530

    Add files via upload

---
+++ Employee/src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee 
+{
+	public String name;
+	public int age;
+	public String city;
+	public void display()
+	{
+		System.out.println("The name is "+name);
+		System.out.println("The age is "+age);
+		System.out.println("The city is "+city);
+	 }
+}
---
+++ Employee/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1=new Employee();
+		e1.name="Saurab";
+		e1.age=23;
+		e1.city="chennai";
+		e1.display();
+		Employee e2=new Employee();
+		e2.name="Mohan";
+		e2.age=29;
+		e2.city="bangalore";
+		e2.display();
+
+	}
+
+}
